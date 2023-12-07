9206f3f16a71e1212672805f1d0c5031a8506b46
LearningToCode1212 <124621733+LearningToCode1212@users.noreply.github.com>
2023-12-07 12:51:54 +0200

    First Commit

---
+++ Assignment2Slnn/Assignment2/ConvertToText.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    internal class ConvertToText
+    {
+        public static string convert(int input)
+        {
+            string value = input.ToString();
+            return value;
+        }
+    }
+}
---
+++ Assignment2Slnn/Assignment2/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Assignment2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string value = ConvertToText.convert(12);
+            Console.WriteLine(value);
+        }
+    }
+}
